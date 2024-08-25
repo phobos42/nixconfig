414c49fb5afd2f89dfb1cb7bed598c3389a39165
phobos42 <garrettruffner42@gmail.com>
2024-08-25 15:48:34 -0500

    Syncthing update

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -7,10 +7,27 @@
     settings = {
       gui = {
         insecureSkipHostcheck = true;
-        enabled="true";
-        tls="false";
-        debugging="true";
-        address="0.0.0.0:8384";
+        enabled = true;
+        tls = false;
+        debugging = true;
+        address = "0.0.0.0:8384";
+      };
+      folders = {
+        Vortex = {
+          versioning.type = "simple";
+          path = "~/Vortex";
+          label = "Vortex";
+          id = "yzejp-g3vhs";
+          enable = true;
+          devices = [ "bigmac" ];
+        };
+      };
+      devices = {
+        bigmac = {
+          name = "bigmac";
+          id = "4U3PFOX-TNPSVSJ-IX36QOC-CCOPF5L-XXKHXBM-4EDFFZS-67FCLLO-FRJVIAZ";
+          autoAcceptFolders = true;
+        };
       };
     };
 
