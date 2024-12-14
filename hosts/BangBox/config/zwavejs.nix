fb7e6073eba185df1a2dba4cbc81706bb4554fe4
phobos42 <garrettruffner42@gmail.com>
2024-12-14 17:58:35 -0600

    Add Restart Timer for Zwave js unit

--- hosts/BangBox/config/zwavejs.nix
+++ hosts/BangBox/config/zwavejs.nix
@@ -15,4 +15,24 @@
       mode = "0444";
     };
   };
+
+  systemd.timers."restart-zwavejs" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 3:00:00";
+      Persistent = true;
+      Unit = "restart-zwavejs.service";
+    };
+  };
+
+  systemd.services."restart-zwavejs" = {
+    script = ''
+      set -eu
+      systemctl restart zwave-js.service
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
 }
