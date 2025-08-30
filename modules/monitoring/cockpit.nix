3893452e66c296ce4ed1d400fd035597b578873f
phobos42 <garrettruffner42@gmail.com>
2025-08-30 12:31:49 -0500

    intermediate update

--- modules/monitoring/cockpit.nix
+++ modules/monitoring/cockpit.nix
@@ -1,16 +1,35 @@
-{ pkgs-unstable, ... }:
-let portNumber = 9090;
+# { pkgs-unstable, ... }:
+# let portNumber = 9090;
+# in {
+#   services.cockpit = {
+#     package = pkgs-unstable.cockpit;
+#     enable = true;
+#     port = portNumber;
+#     settings = {
+#       "WebService" = {
+#         Origins =
+#           "https://cockpit.home.garrettruffner.com https://cockpit.tailnethome.garrettruffner.com";
+#       };
+#     };
+#   };
+#   services.traefik-wrapper.service-definitions = builtins.listToAttrs [{
+#     name = "cockpit";
+#     value = { url = "http://127.0.0.1:${toString portNumber}"; };
+#   }];
+# }
+{ pkgs-unstable, config, ... }:
+let
+  portNumber = 9090;
+  cfg = config.services.traefik-wrapper;
 in {
   services.cockpit = {
     package = pkgs-unstable.cockpit;
     enable = true;
     port = portNumber;
-    settings = {
-      "WebService" = {
-        Origins =
-          "https://cockpit.home.garrettruffner.com https://cockpit.tailnethome.garrettruffner.com";
-      };
-    };
+    allowed-origins = 
+      builtins.map (subdomain:
+        ''https://cockpit.${subdomain}.${cfg.base-domain}'')
+        cfg.domain-subnets;
   };
   services.traefik-wrapper.service-definitions = builtins.listToAttrs [{
     name = "cockpit";
